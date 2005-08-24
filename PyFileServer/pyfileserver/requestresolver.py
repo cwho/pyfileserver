7508871816320798e5f7f43d6c1d1cdb0b2583df
Ian Bicking <ian@ianbicking.org>
2005-08-24 04:08:48 +0000

    Feedback/code review on module
    git-svn-id: svn://svn.berlios.de/pyfilesync/branches/code-review@29 d9fca66e-66fb-0310-a289-d985befa7510

--- PyFileServer/pyfileserver/requestresolver.py
+++ PyFileServer/pyfileserver/requestresolver.py
@@ -1,3 +1,7 @@
+# @@: I think this module in general could be more generally seen as a case
+#     of dispatching different URL prefixes to different applications.
+#     Continued below...
+
 """
 requestresolver
 ===============
@@ -61,6 +65,47 @@ placed in the environ dictionary::
    environ['pyfileserver.mappedpath'] = /home/public/share/PyFileServer/LICENSE 
    environ['pyfileserver.mappedURI'] = /pubshare/PyFileServer/LICENSE
 
+@@: If this were just generalized URL mapping, you'd map it like:
+    Incoming:
+        SCRIPT_NAME=<approot>; PATH_INFO=/pubshare/PyFileServer/LICENSE
+    After transforamtion:
+        SCRIPT_NAME=<approot>/pubshare; PATH_INFO=/PyFileServer/LICENSE
+    Then you dispatch to the application that serves '/home/public/share/'
+
+    This uses SCRIPT_NAME and PATH_INFO exactly how they are intended to be
+    used -- they give context about where you are (SCRIPT_NAME) and what you
+    still have to handle (PATH_INFO)
+
+    An example of an dispatcher that does this is paste.urlmap, and you use it
+    like:
+
+      urlmap = paste.urlmap.URLMap()
+      # urlmap is a WSGI application
+      urlmap['/pubshare'] = PyFileServerForPath('/home/public/share')
+
+    Now, that requires that you have a server that is easily
+    instantiated, but that's kind of a separate concern -- what you
+    really want is to do more general configuration at another level.  E.g.,
+    you might have::
+
+      app = config(urlmap, config_file)
+
+    Which adds the configuration from that file to the request, and
+    PyFileServerForPath then fetches that configuration.  paste.deploy
+    has another way of doing that at instantiation-time; either way
+    though you want to inherit configuration you can still use more general
+    dispatching.
+
+    Incidentally some WebDAV servers do redirection based on the user
+    agent (Zope most notably).  This is because of how WebDAV reuses
+    GET in an obnxious way, so that if you want to use WebDAV on pages
+    that also include dynamic content you have to mount the whole
+    thing at another point in the URL space, so you can GET the
+    content without rendering the dynamic parts.  I don't actually
+    like using user agents -- I'd rather mount the same resources at
+    two different URLs -- but it's just an example of another kind of
+    dispatching that can be done at a higher level.
+
 The resolver also resolves any relative paths to its canonical absolute path
 
 The RequestResolver also resolves any value in the Destination request 
@@ -100,25 +145,52 @@ from processrequesterrorhandler import HTTPRequestException
 import websupportfuncs
 import httpdatehelper
 
+# @@: This is a constant that can never change, since / will always be the URL separator.
+#     I think embedding the character in the code is actually better in this case.
 # CONSTANTS
 URL_SEP = '/'
         
 def resolveRealmURI(mapcfg, requestpath):
     requestpath = urllib.unquote(requestpath)
+    # @@: I don't think you want to sort these alphabetically; instead they should be
+    # sorted by length (longest-first), since the longest paths are the most specific
     mapcfgkeys = mapcfg.keys()
     mapcfgkeys.sort()
     mapcfgkeys.reverse()
     mapdirprefix = ''
     mapdirprefixfound = False
     for tmp_mapdirprefix in mapcfgkeys:
+        # @@: Case sensitivity should be an option of some sort; while it is conventional
+        # on Windows, it's not conventional elsewhere, and generally even Windows will
+        # preserve case so long as you give it the option.  Really if you want to be
+        # case-insensitive it's best to redirect to the canonical case of the resource
+        # (but maybe only for GET, since other operations can't be safely redirected --
+        # but as long as you redirect early it's unlikely someone will access anything
+        # with the wrong case).  I'm not sure, but os.path.normpath might give the
+        # prefered case for a filename.
         if requestpath.upper() == tmp_mapdirprefix.upper() or requestpath.upper().startswith(tmp_mapdirprefix.upper() + URL_SEP):
             mapdirprefixfound = True
             mapdirprefix = tmp_mapdirprefix   
             break
-                
+
+    # @@: I haven't looked at the calling context yet, but generally it's best to raise
+    # an exception in a case like this.
+    # But anyway, you can do something like:
+    #   for try_prefix in mapcfgkeys:
+    #       if try_prefix matches:
+    #           ...
+    #           break
+    #   else:
+    #       (this code gets run when the for loop doesn't terminate with "break")
     if not mapdirprefixfound:
-        return (None, None, None)                  
-    
+        return (None, None, None)
+
+    # There's a possible security risk here, as someone could request "/pubdir/../../foobar".
+    # Browsers won't make this sort of request, but someone could construct a request that
+    # does this.  A simple assertion later on will protect against this; normalize the path
+    # with os.path.normpath, then assert that it starts with the thing you expect it to start
+    # with.  You don't really have to give proper HTTP error messages to hacking attempts,
+    # so letting the AssertionError go uncatched in fine.
     relativepath = requestpath[len(mapdirprefix):]
     localheadpath = mapcfg[mapdirprefix]
 
@@ -213,7 +285,9 @@ class RequestResolver(object):
         elif os.path.isdir(os.path.dirname(mappedpath)):
             start_response('200 OK', [('Content-Type', 'text/html'), ('Content-Length','0'), ('Allow','OPTIONS PUT MKCOL'), ('DAV','1,2'), ('Date',httpdatehelper.getstrftime())])      
         else:
-            raise HTTPRequestException(processrequesterrorhandler.HTTP_NOT_FOUND)         
+            raise HTTPRequestException(processrequesterrorhandler.HTTP_NOT_FOUND)
+        # @@: I'd say that return [''] is generally better, as you don't set up an unnecessary
+        # generator context
         yield ''
         return      
     
@@ -228,5 +302,8 @@ class RequestResolver(object):
 </html>        
         """
         start_response('200 OK', [('Cache-Control','no-cache'), ('Content-Type', 'text/html'), ('Date',httpdatehelper.getstrftime())])
+        # @@: ditto on the return; you should only use yield when you really are doing
+        # extra work on each iteration
         yield message          
         return
+    
