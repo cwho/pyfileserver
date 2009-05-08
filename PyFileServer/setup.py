bd6f171ef2c1fdf2251b4eb0d1ba1f96b150a5c1
Martin Wendt <moogle@wwwendt.de>
2009-05-08 20:55:32 +0000

    Using lxml instead of PyXML now. Only smoke tested, see also TODO.txt.
    git-svn-id: svn://svn.berlios.de/pyfilesync/branches/replace-pyxml@45 d9fca66e-66fb-0310-a289-d985befa7510

--- PyFileServer/setup.py
+++ PyFileServer/setup.py
@@ -13,11 +13,15 @@ import sys
 
 print "\nChecking manual dependencies...\n"
 try:
-   import xml.dom.ext
-   import xml.dom.ext.reader
+#   import xml.dom.ext
+#   import xml.dom.ext.reader
+   from lxml import etree
+   
 except ImportError:
-   print "Failed to detect PyXML. PyXML is required for PyFileServer. Please install"
-   print "PyXML <http://pyxml.sourceforge.net/> before installing PyFileServer"
+#   print "Failed to detect PyXML. PyXML is required for PyFileServer. Please install"
+#   print "PyXML <http://pyxml.sourceforge.net/> before installing PyFileServer"
+   print "Failed to detect lxml. lxml is required for PyFileServer. Please install"
+   print "lxml <http://codespeak.net/lxml/> before installing PyFileServer"
    sys.exit(-1)
 
 
